92817b6f11408c384ee47931449b76d86ea96ad5
vmangal11 <mvidisha06@gmail.com>
2022-03-07 08:37:41 +0530

    update in pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+  
+        <div className="card shadow m-2 p-2 text-primary">Test <div className="btn btn-warning">OK</div></div>
+        
+    </div>    
+  )
 }
